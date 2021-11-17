24158fc5e85b8c55a72f34b5b30756bc9655356d
s-liric <s-liric@51S500123917.it.bsd405.org>
2021-11-17 09:27:39 -0800

    commit

---
+++ src/exerciseApplication/ApplicationJournalAnalysis.java
@@ -0,0 +1,25 @@
+package exerciseApplication;
+
+import java.io.IOException;
+
+public class ApplicationJournalAnalysis extends ApplicationJournal 
+{
+
+	public ApplicationJournalAnalysis() throws IOException {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	/*
+	 - TO DO LIST:
+	 - list out every journal of a certain exercise?
+	 - get the most frequently done exercise?
+	 - change in weight for a certain exercise?
+	 - get all the information for an exercise?
+	 - figure out how much to progressively overload (take the average of everything and add it to the value? take 
+	 - 
+	 
+	 */
+	
+
+}
